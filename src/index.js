c35f3d7daa5a4610901e06d20e9cf4faacd0da89
oldUath <haiyangdong666@gmail.com>
2020-11-12 14:25:41 +0800

    一个useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// initValue是初始值，接收传入的参数n
+let _state;
+const myUseState=(initValue)=>{
+  _state = _state ===undefined?initValue:_state;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+  const setState=(newState)=>{
+    _state = newState;
+   ReactDOM.render(<App />,document.getElementById('root'));
+  };
+
+  return [_state,setState]
+}
+
+
+const App =()=>{
+  const [n,setN] = myUseState(0);
+  return(
+    <div>
+      n:{n}
+      <button onClick={add=>{setN(n+1)}}>+1</button>
+
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
