ef5e78557f67b107d2365f707df67b7aef04852b
oldUath <haiyangdong666@gmail.com>
2020-11-12 15:06:03 +0800

    用数组接收多个变量，控制不同的数据

--- src/index.js
+++ src/index.js
@@ -2,28 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // initValue是初始值，接收传入的参数n
-let _state;
+let _state=[];
+let index=0;
 const myUseState=(initValue)=>{
-  _state = _state ===undefined?initValue:_state;
-
+  const currentIndex = index
+  _state[currentIndex] = _state[currentIndex] ===undefined?initValue:_state[currentIndex];
 
   const setState=(newState)=>{
-    _state = newState;
-   ReactDOM.render(<App />,document.getElementById('root'));
+    _state[currentIndex] = newState;
+   render();
   };
+  index+=1;
+  return [_state[currentIndex],setState]
+};
+const render=()=>{
+  index=0
+  ReactDOM.render(<App />,document.getElementById('root'));
 
-  return [_state,setState]
 }
-
 
 const App =()=>{
   const [n,setN] = myUseState(0);
+  const [m,setM] = myUseState(0);
   return(
-    <div>
-      n:{n}
-      <button onClick={add=>{setN(n+1)}}>+1</button>
+    <>
+      <div>
+        n:{n}
+        <button onClick={()=>{setN(n+1)}}>+1</button>
+      </div>
+      <div>
+        m:{m}
+        <button onClick={()=>{setM(m+1)}}>+1</button>
+      </div>
 
-    </div>
+    </>    
   )
 }
 
