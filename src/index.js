55b93e7177a11125a964ba5344a3e8db685a343b
oldUath <haiyangdong666@gmail.com>
2020-11-15 21:16:58 +0800

    使用useState

--- src/index.js
+++ src/index.js
@@ -1,42 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// initValue是初始值，接收传入的参数n
-let _state=[];
-let index=0;
-const myUseState=(initValue)=>{
-  const currentIndex = index
-  _state[currentIndex] = _state[currentIndex] ===undefined?initValue:_state[currentIndex];
 
-  const setState=(newState)=>{
-    _state[currentIndex] = newState;
-   render();
-  };
-  index+=1;
-  return [_state[currentIndex],setState]
-};
-const render=()=>{
-  index=0
-  ReactDOM.render(<App />,document.getElementById('root'));
-
-}
 
 const App =()=>{
-  const [n,setN] = myUseState(0);
-  const [m,setM] = myUseState(0);
+  const [user,setUser]=useState(({name:'dong',age:18}));
+  const onClick=()=>{
+    setUser({
+      ...user,
+      name:'zhi'
+    })
+  }
   return(
-    <>
-      <div>
-        n:{n}
-        <button onClick={()=>{setN(n+1)}}>+1</button>
-      </div>
-      <div>
-        m:{m}
-        <button onClick={()=>{setM(m+1)}}>+1</button>
-      </div>
+    <div>
+    <h1>{user.name}</h1>
+    <h1>{user.age}</h1>
+    <button onClick={onClick}>onclick</button>
+  </div>    
 
-    </>    
-  )
+  );
 }
 
 ReactDOM.render(<App />,document.getElementById('root'));
