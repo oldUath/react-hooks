fbd2e7c9faba25a35257d0d5209a76e8a975617f
oldUath <haiyangdong666@gmail.com>
2020-11-19 13:25:01 +0800

    useContext

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-import React, { useReducer, useState } from 'react'
+import React, { createContext, useContext, useReducer, useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// 第一步：初始化
-const initial ={
-    n:0
-}
-// 第二步：创建所有操作    state是状态（就是数据） action操作类型
-const reducer = (state,action)=>{
-  if(action.type === 'add'){
-    return {n:state.n+action.number}
-  }else if(action.type === 'mult'){
-    return {n:state.n*action.number}
-  }else{
-    throw new Error('unKnow')
-  }
+// 创造一个上下文
+const C = createContext(null);
+
+function App(){
+  const [n,setN] = useState(0)
+  return(
+    // 指定上下文使用范围，使用provider,并传入读数据和写入据
+    <C.Provider value={{n,setN}}>
+      这是爷爷
+      <Baba></Baba>
+    </C.Provider>
+  )
 }
 
-const App =()=>{
-    // 第三步：state是状态，dispatch是操作  reducer是操作的集合，initial是初始状态
-    const [state,dispatch] = useReducer(reducer,initial);
-
-    const onClick=()=>{
-        dispatch({type:'add',number:1})
-    };
-    const onClick2=()=>{
-        dispatch({type:'mult',number:2})
-    }
+function Baba(){
   return(
     <div>
-      n:{state.n}
-      <button onClick={onClick}>+1</button>
-      <button onClick={onClick2}>*2</button>
-    </div>    
-  );
+      这是爸爸
+      <Child></Child>
+    </div>
+  )
 }
+function Child(){
+  // 使用上下文，因为传入的是对象，则接受也应该是对象
+  const {n,setN} = useContext(C)
+  const add=()=>{
+    setN(n=>n+1)
+  };
+  return(
+    <div>
+      这是儿子:n:{n}
+      <button onClick={add}>+1</button>
+    </div>
+  )
+}
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
