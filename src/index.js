0d6fed8b7cafe422b467ac10a62f02122ea07500
oldUath <haiyangdong666@gmail.com>
2020-11-20 13:10:32 +0800

    memo和useMemo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import React, { useEffect,useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
 function App(){
   const [n,setN] = useState(0);
+  const [m,setM] = useState(0);
   const add=()=>{
     setN(i=>i+1);
   };
-
-// 第一次渲染，只执行这一次，[]要在第二个参数
-useEffect(()=>{
-      console.log('这是第一次渲染执行这句话');
-    },[]);
-// 第一,二，三次渲染，每次都执行，不用写第二个参数
-useEffect(()=>{
-  console.log('这是第1.2.3次渲染执行这句话');
-});
-//当n变化了就执行这句
-  useEffect(()=>{
-    console.log('n变了');
-  },[n])
-
+  const addChild=()=>{
+    setM(i=>i+1);
+  };
+  const onClickChild=useMemo(()=>{
+    return ()=>{
+      console.log(m)
+    }
+  },[m])
 
   return(
     <div>
-      n:{n}
-      <button onClick={add}>+1</button>
+      <div>
+        n:{n}
+        <button onClick={add}>n+1</button>
+        <button onClick={addChild}>m+1</button>
+      </div>
+      <Child2 data={m} onClick={onClickChild} />
     </div>
   )
 }
-
+function Child(props){
+  console.log('child执行了')
+  console.log('这里有很多代码')
+  return(
+    <div onClick={props.onClick}>
+      child:{props.data}
+    </div>
+      )
+}
+// 
+const Child2 = React.memo(Child)
 ReactDOM.render(<App />,document.getElementById('root'));
